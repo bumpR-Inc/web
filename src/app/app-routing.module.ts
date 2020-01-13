1a1a584e6ab20798e9d0d07a440b0022d39dcdc1
Armaan Goel <armaangoel78@gmail.com>
2020-01-13 13:27:11 -0500

    added fade in animation for the form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), BrowserAnimationsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    // trigger('center-to-top', [
+    //   transition('void => *', [
+    //     style({margin: "10em"}),
+    //     animate(4000, style({margin: "10em"})),
+    //     animate(500, style({margin: "5em"}))
+    //   ])
+    // ]),
+    trigger('delayed-fade', [
+      transition('void => *', [
+        style({opacity:0}),
+        animate(3700, style({opacity: 0})),
+        animate(500, style({opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'web';
